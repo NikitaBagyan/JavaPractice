cd7e313058756be943f96296addcb6cae42c5950
Nikita <doarchik@gmail.com>
2022-12-25 14:57:52 +0300

    int format

--- Default/src/Forgotten.java
+++ Default/src/Forgotten.java
@@ -1,2 +1,10 @@
+import java.util.Locale;
 public class Forgotten {
+    public static void main(String[] args) {
+
+
+        int value = 3434436;
+        String str = String.format(Locale.US, "%,d", value);
+        System.out.println(str);
+    }
 }
--- Default/src/Pastyr.java
+++ Default/src/Pastyr.java
@@ -1,5 +1,5 @@
 public class Pastyr {
     public static void main(String[] args) {
-        System.out.println("My house");
+        System.out.println("My house3");
     }
 }
